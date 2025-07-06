c1dcad00be0dabdce063ad59884b307a53d15ab7
Vinicius Juarez <viniciusjuarez720@gmail.com>
2025-07-06 19:51:24 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button, Spinner } from "@av-digital/components"
+import '@av-digital/components/styles'
+function Consumer() {
+  return (
+    <>
+      <Button variant="primary">Button</Button>
+      <Spinner/>
+    </>
+  )
+}
+
+export default Consumer
